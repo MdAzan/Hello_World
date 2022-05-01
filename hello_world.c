3f94422a19c15e87eabcc58a529b02875b4168a6
Mohammed Azan <ahobbanazan@gmail.com>
2022-05-01 09:35:12 +0600

    another commit

--- hello_world.c
+++ hello_world.c
@@ -2,8 +2,17 @@
 
 
 int main() {
-    printf("Hello World\n");
-
+    // pointers
+    int Number = 5000;
+    // print The Number
+    printf("The number is = %d\n", Number);
+    // Address of the Number in memory
+    int *address_of_number = &Number;
+    printf("The address of the number is = %u\n", address_of_number);
+    // Find out what data is stored on this(address_of_number) address
+    int finding_data_by_address = *address_of_number;
+    printf("stored data on addrees_of_number variable is = %d\n", finding_data_by_address); // by a variable
+    printf("stored data on addrees_of_number variable is = %d\n", *address_of_number); // without variable
 
     return 0;
 }
